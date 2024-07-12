1941b40e4611ed1a4489fd5dbc2a400040775c9c
Marcos Nobre <marconobre@gmail.com>
2024-07-11 21:01:53 -0300

    Atualização

---
+++ install4Docker/comando.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -s "https:/laravel.build/example-app" | bash
+
+# P/ MySQL no host
+# mysql -h localhost -u mysqladmin -pmyadmin
+
+# P/ PgSQL no host
+# psql -h localhost -U postgres postgres [senha=>pg01]
+
+
+
