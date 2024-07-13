3525fcda082beadbfa360006c5cf2754c1ca7ffe
Marcos Nobre <marconobre@gmail.com>
2024-07-13 00:01:56 -0300

    Atualização

---
+++ install4Docker/2aAppLaravel/app/Models/SignatureHistory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SignatureHistory extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'signature_id',
+        'last_updated_at',
+        'last_plan_id',
+        'last_status'
+    ];
+
+    protected $casts = [
+        'last_status' => SignatureStatus::class
+    ];
+}
