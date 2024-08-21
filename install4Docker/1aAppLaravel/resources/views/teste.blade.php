fbda2b36e2fd0ecf5f87e2b2c070a2cfacb23d81
Marcos Nobre <marconobre@gmail.com>
2024-08-21 14:49:05 -0300

    Atualização

---
+++ install4Docker/1aAppLaravel/resources/views/teste.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h3>Dados de Cliente</h3>
+    </x-slot>
+    <ol>
+        <li>Nome: {{ auth()->user()->name }}</li>
+        <li>Documento: {{ auth()->user()->client->document }}</li>
+        <li>Status da Assinatura: {{ auth()->user()->client->signatures->first()->status->name }}</li>
+    </ol>
+</x-app-layout>
--- install4Docker/1aAppLaravel/routes/web.php
+++ install4Docker/1aAppLaravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Enums\SignatureStatus;
 use App\Http\Controllers\ProfileController;
+use App\Models\Plan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +21,28 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/testeList', function () {
+    return view('teste');
+});
+
+Route::get('/testeInsert', function () {
+    $plan = Plan::create([
+        'name' => 'Principal Plan',
+        'short_description' => 'Another important plan',
+        'price' => 1999
+    ]);
+
+    $client = Auth::user()->client()->create( [
+        'document' => '00020272077',
+        'birthdate' => '1977-11-25'
+    ]);
+
+    $client->signatures()->create( [
+        'plan_id' => $plan->id,
+        'status' => SignatureStatus::ACTIVED
+    ]);
+
+    return 'Inseridos: Plano, Cliente e Assinatura';
+});
